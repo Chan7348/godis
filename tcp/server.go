66e3284484546e8d1357aca929c7124b35eb6e4f
Drop <add7348@outlook.com>
2024-12-15 14:13:19 +0800

    add

---
+++ tcp/server.go
@@ -0,0 +1,88 @@
+package tcp
+
+import (
+	"context"
+	"net"
+	"os"
+	"os/signal"
+	"sync"
+	"sync/atomic"
+	"syscall"
+	"time"
+
+	"github.com/Chan7348/godis/interface/tcp"
+	"github.com/Chan7348/godis/lib/logger"
+)
+
+type Config struct {
+	Address    string        `yaml:"address"`
+	MaxConnect uint32        `yaml:"max-connect"`
+	Timeout    time.Duration `yaml:"timeout"`
+}
+
+var ClientCounter int32
+
+func ListenAndServeWithSignal(cfg *Config, handler tcp.Handler) error {
+	closeChan := make(chan struct{})
+	sigCh := make(chan os.Signal)
+	signal.Notify(sigCh, syscall.SIGHUP, syscall.SIGQUIT, syscall.SIGTERM, syscall.SIGINT)
+	go func() {
+		sig := <-sigCh
+		switch sig {
+		case syscall.SIGHUP, syscall.SIGQUIT, syscall.SIGTERM, syscall.SIGINT:
+			closeChan <- struct{}{}
+		}
+	}()
+
+	listener, err := net.Listen("tcp", cfg.Address)
+	if err != nil {
+		return err
+	}
+	logger.Infof("bind: %s, start listening...", cfg.Address)
+	ListenAndServe(listener, handler, closeChan)
+	return nil
+}
+
+func ListenAndServe(listener net.Listener, handler tcp.Handler, closeChan <-chan struct{}) {
+	errCh := make(chan error, 1)
+	defer close(errCh)
+
+	go func() {
+		select {
+		case <-closeChan:
+			logger.Info("get exit signal")
+		case er := <-errCh:
+			logger.Infof("accept error: %s", er.Error())
+		}
+		logger.Info("shutting down ...")
+		_ = listener.Close()
+		_ = handler.Close()
+	}()
+
+	ctx := context.Background()
+	var waitDone sync.WaitGroup
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			if ne, ok := err.(net.Error); ok && ne.Timeout() {
+				logger.Infof("accept occurs temporary error: %v, retry in 5ms", err)
+				time.Sleep(5 * time.Millisecond)
+				continue
+			}
+			errCh <- err
+			break
+		}
+
+		logger.Info("accept link")
+		ClientCounter++
+		waitDone.Add(1)
+		go func() {
+			defer func() {
+				waitDone.Done()
+				atomic.AddInt32(&ClientCounter, -1)
+			}()
+			handler.Handle(ctx, conn)
+		}()
+	}
+	waitDone.Wait()
+}
